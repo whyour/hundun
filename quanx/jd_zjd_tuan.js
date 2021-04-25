48db6024cc5a5930f2cc4731bb642271db83be6b
q4w5e6 <51695264+q4w5e6@users.noreply.github.com>
2021-04-25 10:20:35 +0800

    try fix:助力提示活动火爆

--- quanx/jd_zjd_tuan.js
+++ quanx/jd_zjd_tuan.js
@@ -171,9 +171,9 @@ function taskTuanUrl(function_id, body = {}) {
       Connection: 'keep-alive',
       'Content-Type': 'application/x-www-form-urlencoded',
       Host: 'api.m.jd.com',
-      Referer: 'https://servicewechat.com/wxa5bf5ee667d91626/108/page-frame.html',
-      Cookie: $.currentCookie,
-      'User-Agent': 'jdapp;iPhone;9.2.2;14.2;%E4%BA%AC%E4%B8%9C/9.2.2 CFNetwork/1206 Darwin/20.1.0',
+      Referer: 'https://servicewechat.com/wxa5bf5ee667d91626/136/page-frame.html',
+      Cookie: $.currentCookie + 'appkey=797c7d5f8f0f499b936aad5edcffa08c;appid=wxa5bf5ee667d91626;wxclient=gxhwx;ie_ai=1',
+      'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.5(0x18000520) NetType/WIFI Language/zh_CN',
     },
   };
 }
